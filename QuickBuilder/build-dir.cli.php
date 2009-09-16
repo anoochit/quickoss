217042e96fed26dbaada7989cbe7c848284803f1
anoochit <anoochit@e8a539b0-a269-11de-b161-9f508301ba8e>
2009-09-16 08:30:44 +0000

    del src

--- QuickBuilder/build-dir.cli.php
+++ QuickBuilder/build-dir.cli.php
@@ -13,15 +13,19 @@ function make_base_dir($srcfile,$category) {
 				} else {
 					// make item dir
 					list($title,$version,$url)=explode('|', $item);
-					mkdir("src/".$category."/".$title,0777);
-					echo "mkdir src/".$category."/".$title."\n";							
+					mkdir("src/".$category."/".strtolower($title),0777);
+					echo "mkdir src/".$category."/".strtolower($title)."\n";		
+					$cmd="echo '$title $version' >> src/software.list.txt";
+					system($cmd); 	
+					//$cmd="wget -b ".$url." -O src/".$category."/".$title."/".strtolower($title)."-".$version."-setup.exe";
+			    	//system($cmd);   								
 				}
 	    }
 	}
 }
 
 // create src dir
-//mkdir("src",0777);
+mkdir("src",0777);
 
 // create base category
 make_base_dir("src.category.list.txt","");
@@ -29,7 +33,6 @@ make_base_dir("src.category.list.txt","");
 //create program in base dir
 make_base_dir("src.development.list.txt","development");
 make_base_dir("src.education.list.txt","education");
-/*
 make_base_dir("src.game.list.txt","game");
 make_base_dir("src.graphics.list.txt","graphics");
 make_base_dir("src.internet.list.txt","internet");
@@ -37,5 +40,5 @@ make_base_dir("src.multimedia.list.txt","multimedia");
 make_base_dir("src.office.list.txt","office");
 make_base_dir("src.tools.list.txt","tools");
 make_base_dir("src.web.list.txt","web");
-*/
+
 ?>
