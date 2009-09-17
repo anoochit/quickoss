fa11a5b54ecbf4e7cdf0c4cc16c7994f8a898836
anoochit <anoochit@e8a539b0-a269-11de-b161-9f508301ba8e>
2009-09-17 10:17:39 +0000

    update build script

--- trunk/QuickBuilder/build-iso.cli.php
+++ trunk/QuickBuilder/build-iso.cli.php
@@ -1,8 +1,7 @@
 #!/usr/bin/php
 <?php
 
-$cmd="mkisofs -o release/quickpc_v".date(ym.d").iso -graft-points -rational-rock -full-iso9660-filenames -iso-level 2 -r -J 
-src/";
+$cmd="mkisofs -o release/quickpc_v.".date("Ymd").".iso -graft-points -rational-rock -full-iso9660-filenames -iso-level 2 -r -J src/";
 system($cmd); 	
 
 ?>
